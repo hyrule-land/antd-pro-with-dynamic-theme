c5b20355d50c5344b79fa3c322d6ce13d70de8b4
qinhui <932433511@qq.com>
2019-12-16 19:10:52 +0800

    init project, 添加 antd.less 和 less.min.js

---
+++ config/defaultSettings.js
@@ -0,0 +1,35 @@
+export default {
+  navTheme: 'light',
+  primaryColor: 'daybreak',
+  layout: 'topmenu',
+  contentWidth: 'Fixed',
+  fixedHeader: false,
+  autoHideHeader: false,
+  fixSiderbar: false,
+  menu: {
+    locale: true,
+  },
+  title: 'Ant Design Pro',
+  pwa: false,
+  iconfontUrl: '',
+  collapse: true,
+  language: 'zh-CN',
+};
+
+// {
+//   navTheme: 'dark',
+//   // 拂晓蓝
+//   primaryColor: 'daybreak',
+//   layout: 'sidemenu',
+//   contentWidth: 'Fluid',
+//   fixedHeader: false,
+//   autoHideHeader: false,
+//   fixSiderbar: false,
+//   colorWeak: false,
+//   menu: {
+//     locale: true,
+//   },
+//   title: 'Ant Design Pro',
+//   pwa: false,
+//   iconfontUrl: '',
+// }
---
+++ src/pages/demo/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+// import { Icon, Modal } from 'antd';
+
+const Demo = () => <div>this is demo page</div>;
+
+export default Demo;
