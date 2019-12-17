75accb7e76b7f9dd4f3da78c98a01737d8b5ca43
qinhui <932433511@qq.com>
2019-12-17 11:53:21 +0800

    add docs folder

---
+++ docs/p__demo__.0dac1371.async.js
@@ -0,0 +1,16 @@
+(window['webpackJsonp'] = window['webpackJsonp'] || []).push([
+  [7],
+  {
+    dX3V: function(e, t, n) {
+      'use strict';
+      var d = n('g09b');
+      Object.defineProperty(t, '__esModule', { value: !0 }), (t.default = void 0);
+      var u = d(n('q1tI')),
+        a = function() {
+          return u.default.createElement('div', null, 'this is demo page');
+        },
+        i = a;
+      t.default = i;
+    },
+  },
+]);
