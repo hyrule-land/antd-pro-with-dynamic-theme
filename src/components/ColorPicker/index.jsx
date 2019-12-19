dda5f12c9973b976d822528015e7444291f15a1d
qinhui <932433511@qq.com>
2019-12-20 05:47:27 +0800

    modify something

--- src/components/ColorPicker/index.jsx
+++ src/components/ColorPicker/index.jsx
@@ -1,19 +1,91 @@
 import React, { Component } from 'react';
-// import { Popover } from 'antd';
-import { ChromePicker, SketchPicker, PhotoshopPicker } from 'react-color';
-import './index.less';
+import PropTypes from 'prop-types';
+import { Popover } from 'antd';
+import {
+  AlphaPicker, BlockPicker, ChromePicker, CirclePicker,
+  CompactPicker, GithubPicker, HuePicker, MaterialPicker,
+  PhotoshopPicker, SketchPicker, SliderPicker, SwatchesPicker, TwitterPicker } from 'react-color';
+import styles from './index.less';
+import './rewriteStyle.less';
+
+const noop = () => { };
+
+const pickers = {
+  AlphaPicker, BlockPicker, ChromePicker, CirclePicker,
+  CompactPicker, GithubPicker, HuePicker, MaterialPicker,
+  PhotoshopPicker, SketchPicker, SliderPicker, SwatchesPicker, TwitterPicker
+};
 
 // eslint-disable-next-line react/prefer-stateless-function
 class ColorPicker extends Component {
+  static defaultProps = {
+    onChangeComplete: noop,
+    onChange: noop,
+    type: 'PhotoshopPicker',
+    color: '#fff',
+  }
+
+  state = {
+    color: this.props.color,
+  };
+
+  handleChangeComplete = color => {
+    const { onChangeComplete } = this.props;
+    this.setState({ color: color.hex });
+    onChangeComplete(color.hex, color);
+  }
+
+  handleChange = color => {
+    const { onChange } = this.props;
+    this.setState({ color: color.hex });
+    onChange(color.hex, color);
+  }
+
   render() {
-    return (
-      <div>
-        <SketchPicker />
-        <PhotoshopPicker />
-        <ChromePicker />
+    const { color } = this.state;
+    const { type } = this.props;
+
+    const Picker = pickers[type];
+
+    const picker = (
+      <Picker
+        color={color}
+        onChange={this.handleChange}
+        onChangeComplete={this.handleChangeComplete}
+      />
+    )
+
+    const swatch = (
+      <div className={styles.swatch}>
+        <div
+          className={styles.color}
+          style={{
+            background: color,
+          }}
+        />
       </div>
     );
+
+    return (
+      <Popover
+        overlayClassName="color-popover"
+        title={false}
+        trigger="click"
+        content={picker}
+      >
+        {swatch}
+      </Popover>
+    );
   }
+}
+
+ColorPicker.propTypes = {
+  onChangeComplete: PropTypes.func,
+  onChange: PropTypes.func,
+  color: PropTypes.string,
+  type: PropTypes.oneOf(['AlphaPicker', 'BlockPicker', 'ChromePicker', 'CirclePicker',
+    'CompactPicker', 'GithubPicker', 'HuePicker', 'MaterialPicker',
+    'PhotoshopPicker', 'SketchPicker', 'SliderPicker', 'SwatchesPicker', 'TwitterPicker']),
 }
 
 export default ColorPicker;
--- src/pages/demo/index.js
+++ src/pages/demo/index.js
@@ -1,12 +1,62 @@
 import React, { Component } from 'react';
+import { Button, message } from 'antd';
 import ColorPicker from '@/components/ColorPicker/';
+import styles from './index.less';
 
 // eslint-disable-next-line react/prefer-stateless-function
 class Demo extends Component {
+  state = {
+    color: '#fff'
+  }
+
+  test = () => {
+
+  }
+
+  onColorPickerChange = (colorHex, color) => {
+    console.log(colorHex, color);
+    this.setState({
+      color: colorHex,
+    })
+  }
+
+  onChangeComplete = (colorHex, color) => {
+    window.less
+      .modifyVars({
+        '@primary-color': colorHex,
+      })
+      .then(() => {
+        message.success('update primary color successful!!!');
+      })
+      .catch(() => {
+        message.error('Failed to update primary color');
+      });
+  }
+
   render() {
+    const { color } = this.state;
+
     return (
-      <div>
-        <ColorPicker />
+      <div
+        className={styles.container}
+        style={{
+          background: color
+        }}
+      >
+        <Button onClick={this.test}>测试</Button>
+        <ColorPicker
+          type="SketchPicker"
+          color="blue"
+          onChange={this.onColorPickerChange}
+          onChangeComplete={this.onChangeComplete}
+        />
+
+        <ColorPicker
+          type="SwatchesPicker"
+          color="yellow"
+          onChange={this.onColorPickerChange}
+          onChangeComplete={this.onChangeComplete}
+        />
       </div>
     );
   }
